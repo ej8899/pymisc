b9cd814f21e0e59fd42728a7d7eb759d5020955a
ej8899 <ej8899@gmail.com>
2023-10-15 17:19:03 -0400

    update: rename basics of variables

---
+++ 01-hello.py
@@ -0,0 +1,7 @@
+print("hello world");
+print (3+5);
+print("hello " + "world");
+print(7)
+#print(7+"7")
+#print("this works despite typeError above")
+
---
+++ 02-groceries.py
@@ -0,0 +1,13 @@
+eggs=2
+banana=1
+salad=10
+steak=30
+
+print ("breakfast cost:")
+print(eggs + banana)
+
+print ("lunch cost:")
+print(salad)
+
+print ("supper cost:")
+print(steak + eggs)
