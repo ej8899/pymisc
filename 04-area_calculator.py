1d11217132fc85c2cbff4f680280058b0f12ef43
ej8899 <ej8899@gmail.com>
2023-10-15 17:40:44 -0400

    add: input, variables and output in one application

--- 04-area_calculator.py
+++ 04-area_calculator.py
@@ -1,5 +1,15 @@
-x = input("what is side one of rectangle?")
-y = input("what is side two of rectangle?")
+x = input("what is side one of rectangle? ")
+y = input("what is side two of rectangle? ")
 
 print("the area of rectangle is: ");
-print (x * y)
+
+# unlike JS, python won't convert or assume types so below is an error
+# print (x * y)
+
+# below 'works', but it puts the two strings together
+print (x+y)
+
+# below is proper output:
+
+z = int(x) * int(y)
+print (f"Area is: {z}")
