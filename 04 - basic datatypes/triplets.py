92fee4c3c23d3d9b4cd4af7adb86089a7d987f63
ej8899 <ej8899@gmail.com>
2023-11-05 14:53:15 -0500

    add: string methods

--- 04 - basic datatypes/triplets.py
+++ 04 - basic datatypes/triplets.py
@@ -1,5 +1,14 @@
-name1 = input ("enter the first name: ")
-name2 = input ("enter the second name: ")
-name3 = input ("enter the third name: ")
+# name1 = input ("enter the first name: ")
+# name1_cap = name1.strip().capitalize();  # note the order of chaining methods
+# name2 = input ("enter the second name: ")
+# name2_cap = name2.capitalize().strip();
+# name3 = input ("enter the third name: ")
+# name3_cap = name3.capitalize().strip();
 
-print(f"The names are {name1}, {name2}, {name3}")
+# print(f"The names are {name1_cap}, {name2_cap}, {name3_cap}")
+
+# c = 4
+# print(c.capitalize())
+
+x = input("give a number to multiply by 10: ").replace(',','')
+print (int(x) * 10)
